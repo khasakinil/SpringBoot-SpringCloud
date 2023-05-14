c7ded74e6b1c03b22d0354cda5fb170574a287ba
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-14 05:33:17 +0530

    Add files via upload

--- currency-exchange-service/src/main/java/com/microservices/v2/currencyexchangeservice/circuitbreaker/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/v2/currencyexchangeservice/circuitbreaker/CircuitBreakerController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
@@ -18,10 +16,10 @@ public class CircuitBreakerController {
 				LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
 	//@RateLimiter(name="default")
-	@Bulkhead(name="sample-api")
+//	@Bulkhead(name="sample-api")
 	//10s => 10000 calls to the sample api
 	public String sampleApi() {
 		logger.info("Sample api call received");
